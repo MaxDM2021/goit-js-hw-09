6bb5e87ee93a6759229aaf65ed347f96dc233565
Maksym Dmitriiev <maximdm22@gmail.com>
2022-08-15 20:38:37 +0300

    222

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+class Timer {
+  constructor() {
+    this.intervalId = null;
+    this.isActive = false;
+  }
+
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    // startBtn.classList.toggle('.clicked');
+
+    this.intervalId = setInterval(() => {
+      const color = this.getRandomHexColor();
+      document.body.style.backgroundColor = color;
+    }, 1000);
+  }
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+    const color = this.getRandomHexColor();
+  }
+
+  getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+}
+
+const timer = new Timer({});
+
+refs.startBtn.addEventListener('click', timer.start.bind(timer));
+refs.stopBtn.addEventListener('click', timer.stop.bind(timer));
